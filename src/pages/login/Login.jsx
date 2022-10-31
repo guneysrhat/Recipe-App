2384249ba18711a6909c996727ad8185c88dc21b
guneysrhat <guneysrhat@gmail.com>
2022-10-31 21:42:09 +0300

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,42 @@
 import React from "react";
-import LoginContainer from "./Login.style";
+import {
+  LoginContainer,
+  FormContainer,
+  StyledImg,
+  Header,
+  StyledForm,
+  StyledInput,
+  StyledButton,
+} from "./Login.style";
+import meal from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <LoginContainer></LoginContainer>;
+  const navigate = useNavigate();
+
+  const userInfo = {
+    username: "test",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate("-1");
+  };
+
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>{"<Vultus/>"}Recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="Enter Username" type="text" />
+          <StyledInput placeholder="Enter Password" type="password" />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
