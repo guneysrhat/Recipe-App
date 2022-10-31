91ca60f1b4200750618d03a1c9f474064f7323d3
guneysrhat <guneysrhat@gmail.com>
2022-10-31 22:02:30 +0300

    theme

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./Navbar.style";
+import { Nav, Brand } from "./Navbar.style";
 const Navbar = () => {
   return (
-    <nav>
-      <a href="/">
+    <Nav justify="space-between" wrap="wrap">
+      <Brand to="">
         <i>{"<Vultus/>"}</i>
         <span>Recaipe</span>
-      </a>
+      </Brand>
       <div>
         <Link to="/">Home</Link>
         <Link to="about">About</Link>
         <Link to="register">Register</Link>
         <Link to="logout">Logout</Link>
       </div>
-    </nav>
+    </Nav>
   );
 };
 
