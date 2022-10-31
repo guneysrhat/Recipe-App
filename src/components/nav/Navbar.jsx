2ae24ae68598bef68dce83e8431b0113abbfffc6
guneysrhat <guneysrhat@gmail.com>
2022-10-31 23:01:11 +0300

    loagout

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -16,11 +16,13 @@ const Navbar = () => {
       <Hamburger onClick={() => setIsOpen(!isOpen)}>
         <GiHamburgerMenu />
       </Hamburger>
-      <Menu isOpen={isOpen}>
+      <Menu isOpen={isOpen} onClick={() => setIsOpen(false)}>
         <MenuLink to="/">Home</MenuLink>
         <MenuLink to="about">About</MenuLink>
         <MenuLink to="register">Register</MenuLink>
-        <MenuLink to="logout">Logout</MenuLink>
+        <MenuLink to="logout" onClick={() => sessionStorage.clear()}>
+          Logout
+        </MenuLink>
       </Menu>
     </Nav>
   );
