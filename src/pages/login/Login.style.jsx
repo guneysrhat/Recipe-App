506b534b328dec5081b89e2e1728e07d9e5f360f
guneysrhat <guneysrhat@gmail.com>
2022-10-31 21:01:14 +0300

    LoginContainer

---
+++ src/pages/login/Login.style.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import Flex from "../../components/globalStyles/Flex.style";
+
+const LoginContainer = styled(Flex)`
+  height: 100vh;
+  background-image: url("https://picsum.photos/1600/900");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
+export default LoginContainer;
