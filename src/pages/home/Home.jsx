9cbbf4561b173d126873a5a937b52d76e192736d
guneysrhat <guneysrhat@gmail.com>
2022-11-01 21:35:27 +0300

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,25 +1,36 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
+import Cards from "../../components/cards/Cards";
+import { HomeImg, ImgDiv } from "./Home.style";
+import HomeSvg from "../../assets/home.svg";
 
 const Home = () => {
   const APP_ID = "7f7f9891";
   const APP_KEY = "eb8cae3b0f8da5c9d3648e6a013be9c4";
   const [query, setQuery] = useState("egg");
   const [selectedMeal, setSelectedMeal] = useState("breakfast");
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState();
   const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
   const getData = async () => {
-    const { data } = await axios(url);
-    setRecipes(data.hits);
+    if (query) {
+      try {
+        const { data } = await axios(url);
+        setRecipes(data.hits);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      alert("Fill the forms");
+    }
   };
   console.log(recipes);
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
   return (
     <div>
@@ -29,6 +40,12 @@ const Home = () => {
         mealType={mealType}
         getData={getData}
       />
+      {!recipes && (
+        <ImgDiv>
+          <HomeImg src={HomeSvg} />
+        </ImgDiv>
+      )}
+      <Cards />
     </div>
   );
 };
